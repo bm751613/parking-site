1185e11121960f93c3d15231626140f10dd3d07f
Brady Menegay <bradymenegay@gmail.com>
2016-10-05 21:38:02 +0000

    scroll

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-$(function(){
-    var controller = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onLeave'
-        }
-    });
-    
-    var slides = $('div');
-    
-    for(var i=0; i<slides.length; i++) {
-        new ScrollMagic.Scene({
-            triggerElement: slides[i],
-            // duration: 500
+(function() {
+	var pTags = document.getElementsByTagName("p");
+	var ctrl = new ScrollMagic.Controller({globalSceneOptions: {}});
+
+	for(var i=0; i<pTags.length; i++) {
+	    var fadein_tween = TweenMax.to('p', .375,{ opacity: 1 });
+        var fadeout_tween = TweenMax.to('p', .375,{ opacity: 0 });
+
+	    new ScrollMagic.Scene({
+          triggerElement: pTags[i],
+          reverse: true
         })
-        .setPin(slides[i])
-        // .addIndicators()
+        .setTween(fadein_tween)
         .addTo(controller);
-    }
-});
+	    
+		new ScrollMagic.Scene({
+          triggerElement: pTags[i],
+          reverse: true
+        })
+        .setTween(fadeout_tween)
+        .addTo(controller);
+	}
+}) ();
